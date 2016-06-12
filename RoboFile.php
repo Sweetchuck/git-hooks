3f0a80c38e89b5a7c01449eeb9db3f3f56cd2723
Andor Dávid <andor.david@cheppers.com>
2016-06-12 14:07:59 +0200

    Setup Phpcs

--- RoboFile.php
+++ RoboFile.php
@@ -4,54 +4,205 @@
  * Robo task definitions for cheppers/git-hooks-robo.
  */
 
-use Cheppers\Robo\Task\GitHooksRobo\LoadTasks as GHRLoadTasks;
-use Robo\Config;
 use Robo\Tasks;
+use Symfony\Component\Process\Process;
 
 /**
  * Class RoboFile.
  */
-class RoboFile extends Tasks {
+// @codingStandardsIgnoreStart
+class RoboFile extends Tasks
+    // @codingStandardsIgnoreEnd
+{
 
-  use GHRLoadTasks;
+    /**
+     * The "bin-dir" configured in composer.json.
+     *
+     * @todo This could be dynamic with `composer config bin-dir`.
+     *
+     * @var string
+     */
+    protected $binDir = 'vendor/bin';
 
-  /**
-   * RoboFile constructor.
-   */
-  public function __construct() {
-    // @todo Make sure this is the right way to add services.
-    /** @var \League\Container\ContainerInterface $c */
-    $c = Config::getContainer();
-    $c->addServiceProvider(GHRLoadTasks::getGitHooksRoboServices());
-  }
+    /**
+     * @var string
+     */
+    protected $gitExecutable = 'git';
 
-  public function deployGitHooks() {
-    $this
-      ->taskGitHooksRoboDeploy()
-      ->run();
-  }
+    /**
+     * @var string[]
+     */
+    protected $filesToDeploy = [
+        '_common',
+        'applypatch-msg',
+        'commit-msg',
+        'post-applypatch',
+        'post-checkout',
+        'post-commit',
+        'post-merge',
+        'post-receive',
+        'post-rewrite',
+        'post-update',
+        'pre-applypatch',
+        'pre-auto-gc',
+        'pre-commit',
+        'pre-push',
+        'pre-rebase',
+        'pre-receive',
+        'prepare-commit-msg',
+        'push-to-checkout',
+        'update',
+    ];
 
-  public function test() {
-    $this->composerValidate();
-    $this->behat();
-  }
+    public function deployGitHooks()
+    {
+        $this->stopOnFail(true);
+        $task = $this->getTaskDeployGitHooks();
+        if ($task) {
+            $task->run();
+        }
+    }
 
-  public function behat() {
-    $this->stopOnFail(TRUE);
-    $this
-      ->taskExec('./vendor/bin/behat --colors --strict')
-      ->run();
-  }
+    public function test()
+    {
+        $this->composerValidate();
+        $this->behat();
+    }
 
-  public function composerValidate() {
-    $this->stopOnFail(TRUE);
-    $this
-      ->taskExec('composer validate')
-      ->run();
-  }
+    public function behat()
+    {
+        $this->stopOnFail(true);
+        $this
+            ->taskExec('./vendor/bin/behat --colors --strict')
+            ->run();
+    }
 
-  public function githookPreCommit() {
-    $this->composerValidate();
-  }
+    public function composerValidate()
+    {
+        $this->stopOnFail(true);
+        $this
+            ->getTaskComposerValidate()
+            ->run();
+    }
 
+    public function lint()
+    {
+        //$this->stopOnFail(true);
+
+        /** @var \Robo\Collection\Collection $c */
+        $c = $this->collection();
+        $c
+            ->add($this->getTaskPhpcsLint())
+            ->add($this->getTaskComposerValidate())
+            ->run();
+    }
+
+    public function lintPhpcs()
+    {
+        $this
+            ->getTaskPhpcsLint()
+            ->run();
+    }
+
+    public function githookPreCommit()
+    {
+        $this->composerValidate();
+    }
+
+    /**
+     * @return \Robo\Task\Base\Exec
+     */
+    protected function getTaskPhpcsLint()
+    {
+        $cmd_pattern = '%s --standard=%s --ignore=%s %s %s';
+        $cmd_args = [
+            escapeshellcmd("{$this->binDir}/phpcs"),
+            escapeshellarg('PSR2'),
+            escapeshellarg('fixtures/project-template/*/vendor/'),
+            escapeshellarg('fixtures/project-template/'),
+            escapeshellarg('RoboFile.php'),
+        ];
+
+        return $this->taskExec(vsprintf($cmd_pattern, $cmd_args));
+    }
+
+    /**
+     * @return \Robo\Task\Base\Exec
+     */
+    protected function getTaskComposerValidate()
+    {
+        return $this->taskExec('composer validate');
+    }
+
+    /**
+     * @return \Robo\Task\Filesystem\FilesystemStack|null
+     */
+    protected function getTaskDeployGitHooks()
+    {
+        $current_dir = realpath(getcwd());
+        $repo_type = $this->getGitRepoType();
+        if ($repo_type === null) {
+            // This directory is not tracked by Git.
+            return null;
+        }
+
+        $git_dir = $this->getGitDir();
+        if (!($repo_type === 'bare' && strpos($current_dir, $git_dir) === 0)
+            && !($repo_type === 'not-bare' && file_exists("$current_dir/.git"))
+        ) {
+            // Git directory cannot be detected 100%.
+            return null;
+        }
+
+        /** @var \League\Container\Container $container */
+        $container = $this->getContainer();
+
+        /** @var \Robo\Task\Filesystem\FilesystemStack $fsStack */
+        $fsStack = $container->get('taskFilesystemStack');
+
+        $git_dir = preg_replace('@^' . preg_quote("$current_dir/", '@') . '@', './', $git_dir);
+        foreach ($this->filesToDeploy as $file_name) {
+            $fsStack->copy("./$file_name", "$git_dir/hooks/$file_name");
+        }
+
+        return $fsStack;
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getGitRepoType()
+    {
+        $cmd = sprintf(
+            '%s rev-parse --is-bare-repository',
+            escapeshellcmd($this->gitExecutable)
+        );
+
+        $process = new Process($cmd);
+        $exit_code = $process->run();
+        if ($exit_code) {
+            return null;
+        }
+
+        return trim($process->getOutput()) === 'true' ? 'bare' : 'not-bare';
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getGitDir()
+    {
+        $cmd = sprintf(
+            '%s rev-parse --git-dir',
+            escapeshellcmd($this->gitExecutable)
+        );
+
+        $process = new Process($cmd);
+        $exit_code = $process->run();
+        if ($exit_code !== 0) {
+            return null;
+        }
+
+        return realpath(rtrim($process->getOutput(), "\n"));
+    }
 }
--- fixtures/project-template/basic/RoboFile.php
+++ fixtures/project-template/basic/RoboFile.php
@@ -6,164 +6,180 @@
 
 use Robo\Result;
 use Robo\Task\BaseTask;
+use Robo\Tasks;
 
 /**
  * Class RoboFile.
  */
-class RoboFile extends Robo\Tasks {
+// @codingStandardsIgnoreStart
+class RoboFile extends Tasks
+    // @codingStandardsIgnoreEnd
+{
+    use \PredestinedLoadTasks;
 
-  use \PredestinedLoadTasks;
+    public function githookPreCommit()
+    {
+        $this->say(__METHOD__ . ' is called');
 
-  public function githookPreCommit() {
-    $this->say(__METHOD__ . ' is called');
-
-    $output = [];
-    $exit_code = NULL;
-    exec('git diff --cached --name-only', $output, $exit_code);
-    $this->stopOnFail(TRUE);
-    $this
-      ->taskPredestined(!$exit_code && !in_array('false.txt', $output))
-      ->run();
-  }
-
-  public function githookPostCommit() {
-    $this->say(__METHOD__ . ' is called');
-  }
-
-  /**
-   * @param string $base_branch
-   * @param string|null $subject_branch
-   */
-  public function githookPreRebase($base_branch, $subject_branch = NULL) {
-    $current_branch = $this->gitCurrentBranch();
-    $this->say(__METHOD__ . ' is called');
-    $this->say(sprintf('Current branch: "%s"', $current_branch));
-    $this->say(sprintf('Upstream: "%s"', $base_branch));
-    $this->say(sprintf('Subject branch: "%s"', $subject_branch));
-
-    if (!$subject_branch) {
-      $subject_branch = $current_branch;
+        $output = [];
+        $exit_code = null;
+        exec('git diff --cached --name-only', $output, $exit_code);
+        $this->stopOnFail(true);
+        $this
+            ->taskPredestined(!$exit_code && !in_array('false.txt', $output))
+            ->run();
     }
 
-    $this->stopOnFail(TRUE);
-    $this
-      ->taskPredestined(($subject_branch !== 'protected'))
-      ->run();
-  }
-
-  /**
-   * @param string $remote_name
-   * @param string $remote_uri
-   */
-  public function githookPrePush($remote_name, $remote_uri) {
-    $this->say(__METHOD__ . ' is called');
-    $this->say("Remote name: $remote_name");
-    $this->say("Remote URI: $remote_uri");
-
-    $git_log_pattern = 'git log --format=%s -n 1 %s';
-    $valid = TRUE;
-    $num_of_lines = 0;
-    while ($line = fgets(STDIN)) {
-      $num_of_lines++;
-      list($local_ref, $local_sha) = explode(' ', $line);
-      if ($valid && $this->gitRefIsBranch($local_ref)) {
-        $cmd = sprintf(
-          $git_log_pattern,
-          escapeshellarg('%B'),
-          escapeshellarg($local_sha)
-        );
-
-        $commit_message = $this
-          ->taskExec($cmd)
-          ->printed(FALSE)
-          ->run()
-          ->getMessage();
-
-        $valid = (trim($commit_message) != 'Invalid');
-      }
+    public function githookPostCommit()
+    {
+        $this->say(__METHOD__ . ' is called');
     }
 
-    $this->say("Lines in stdInput: $num_of_lines");
+    /**
+     * @param string $base_branch
+     * @param string|null $subject_branch
+     */
+    public function githookPreRebase($base_branch, $subject_branch = null)
+    {
+        $current_branch = $this->gitCurrentBranch();
+        $this->say(__METHOD__ . ' is called');
+        $this->say(sprintf('Current branch: "%s"', $current_branch));
+        $this->say(sprintf('Upstream: "%s"', $base_branch));
+        $this->say(sprintf('Subject branch: "%s"', $subject_branch));
 
-    $this->stopOnFail(TRUE);
-    $this
-      ->taskPredestined($valid)
-      ->run();
-  }
+        if (!$subject_branch) {
+            $subject_branch = $current_branch;
+        }
 
-  /**
-   * @param string $is_squash
-   */
-  public function githookPostMerge($is_squash) {
-    $this->say(__METHOD__ . ' is called');
-    $this->say("Squash: $is_squash");
-  }
+        $this->stopOnFail(true);
+        $this
+            ->taskPredestined(($subject_branch !== 'protected'))
+            ->run();
+    }
 
-  /**
-   * @param string $ref
-   *
-   * @return bool
-   */
-  protected function gitRefIsBranch($ref) {
-    return strpos($ref, 'refs/heads/') === 0;
-  }
+    /**
+     * @param string $remote_name
+     * @param string $remote_uri
+     */
+    public function githookPrePush($remote_name, $remote_uri)
+    {
+        $this->say(__METHOD__ . ' is called');
+        $this->say("Remote name: $remote_name");
+        $this->say("Remote URI: $remote_uri");
 
-  /**
-   * @return string
-   */
-  protected function gitCurrentBranch() {
-    $result = $this
-      ->taskExec('git rev-parse --abbrev-ref HEAD')
-      ->printed(FALSE)
-      ->run();
+        $git_log_pattern = 'git log --format=%s -n 1 %s';
+        $valid = true;
+        $num_of_lines = 0;
+        while ($line = fgets(STDIN)) {
+            $num_of_lines++;
+            list($local_ref, $local_sha) = explode(' ', $line);
+            if ($valid && $this->gitRefIsBranch($local_ref)) {
+                $cmd = sprintf(
+                    $git_log_pattern,
+                    escapeshellarg('%B'),
+                    escapeshellarg($local_sha)
+                );
 
-    return trim($result->getMessage());
-  }
+                $commit_message = $this
+                    ->taskExec($cmd)
+                    ->printed(false)
+                    ->run()
+                    ->getMessage();
 
+                $valid = (trim($commit_message) != 'Invalid');
+            }
+        }
+
+        $this->say("Lines in stdInput: $num_of_lines");
+
+        $this->stopOnFail(true);
+        $this
+            ->taskPredestined($valid)
+            ->run();
+    }
+
+    /**
+     * @param string $is_squash
+     */
+    public function githookPostMerge($is_squash)
+    {
+        $this->say(__METHOD__ . ' is called');
+        $this->say("Squash: $is_squash");
+    }
+
+    /**
+     * @param string $ref
+     *
+     * @return bool
+     */
+    protected function gitRefIsBranch($ref)
+    {
+        return strpos($ref, 'refs/heads/') === 0;
+    }
+
+    /**
+     * @return string
+     */
+    protected function gitCurrentBranch()
+    {
+        $result = $this
+            ->taskExec('git rev-parse --abbrev-ref HEAD')
+            ->printed(false)
+            ->run();
+
+        return trim($result->getMessage());
+    }
 }
 
 /**
  * Class PredestinedLoadTasks.
  */
-trait PredestinedLoadTasks {
+// @codingStandardsIgnoreStart
+trait PredestinedLoadTasks
+    // @codingStandardsIgnoreEnd
+{
 
-  /**
-   * @param bool $outcome
-   *
-   * @return \PredestinedTask
-   */
-  public function taskPredestined($outcome) {
-    return new PredestinedTask($outcome);
-  }
-
+    /**
+     * @param bool $outcome
+     *
+     * @return \PredestinedTask
+     */
+    public function taskPredestined($outcome)
+    {
+        return new PredestinedTask($outcome);
+    }
 }
 
 /**
  * Class PredestinedTask.
  */
-class PredestinedTask extends BaseTask {
+// @codingStandardsIgnoreStart
+class PredestinedTask extends BaseTask
+    // @codingStandardsIgnoreEnd
+{
 
-  /**
-   * @var bool
-   */
-  protected $outcome = TRUE;
+    /**
+     * @var bool
+     */
+    protected $outcome = true;
 
-  /**
-   * PredestinedTask constructor.
-   *
-   * @param bool $outcome
-   */
-  public function __construct($outcome) {
-    $this->outcome = $outcome;
-  }
+    /**
+     * PredestinedTask constructor.
+     *
+     * @param bool $outcome
+     */
+    public function __construct($outcome)
+    {
+        $this->outcome = $outcome;
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function run() {
-    return $this->outcome ?
-      Result::success($this, 'True as expected')
-      : Result::error($this, 'False as expected');
-  }
-
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        return $this->outcome ?
+            Result::success($this, 'True as expected')
+            : Result::error($this, 'False as expected');
+    }
 }
